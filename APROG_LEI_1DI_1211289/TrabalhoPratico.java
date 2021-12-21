ae1e5a858f6bb3f345d38bcf7a14bbe60bef9966
Tomás Lopes <tomas050302@gmail.com>
2021-12-21 09:44:49 +0000

    fix: fixed cell mobilezed dirt

--- APROG_LEI_1DI_1211289/TrabalhoPratico.java
+++ APROG_LEI_1DI_1211289/TrabalhoPratico.java
@@ -8,24 +8,25 @@ public class TrabalhoPratico {
   static final int MAX_X_INDEX = 1;
   static final int MAX_Y_INDEX = 2;
 
-  private static int[][] readInput() throws FileNotFoundException {
-    Scanner scanner = new Scanner(new File("sampleInput1.txt"));
+  static final String FILE_PATH = "sampleInput2.txt";
 
-    // Read the description of the terrain
-    scanner.nextLine();
+  private static boolean readInput(int[][] matrix, int rows, int cols, Scanner scanner) throws FileNotFoundException {
+    int counter = 0;
 
-    int rows = scanner.nextInt();
-    int cols = scanner.nextInt();
-
-    int[][] matrix = new int[rows][cols];
-
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows && scanner.hasNextInt(); i++) {
+      for (int j = 0; j < cols && scanner.hasNextInt(); j++) {
         matrix[i][j] = scanner.nextInt();
+        counter++;
       }
     }
 
-    return matrix;
+    // Check if all the positions were filled
+    if (scanner.hasNextInt())
+      return false;
+
+    scanner.close();
+
+    return counter == rows * cols;
   }
 
   private static void printMatrix(int[][] matrix) {
@@ -153,11 +154,15 @@ public class TrabalhoPratico {
     return -1;
   }
 
+  private static boolean hasValidDimensionsToFloodCube(int[][] matrix) {
+    return (matrix.length < CUBE_SIZE || matrix[0].length < CUBE_SIZE);
+  }
+
   private static int calculateCellMobilizedDirt(int[][] matrix, int x, int y) {
     int cellMobilizedDirt = 0;
 
     if (isFlooded(matrix[x][y]))
-      return cellMobilizedDirt = matrix[x][y] + CUBE_SIZE;
+      cellMobilizedDirt = Math.abs(matrix[x][y]) - CUBE_SIZE;
     else
       cellMobilizedDirt = matrix[x][y] + CUBE_SIZE;
 
@@ -178,9 +183,11 @@ public class TrabalhoPratico {
     int minMobilizedDirt = calculateMobilizedDirtToFloodCube(matrix, 0, 0);
     int maxX = 0, maxY = 0;
 
-    // TODO: Refactor because (0,0) is checked twice
     for (int i = 0; i < matrix.length - CUBE_SIZE + 1; i++) {
       for (int j = 0; j < matrix[0].length - CUBE_SIZE + 1; j++) {
+        if (i == 0 && j == 0)
+          continue;
+
         int mobilizedDirt = calculateMobilizedDirtToFloodCube(matrix, i, j);
 
         // As is used > instead of >=, the selected cell is both the
@@ -197,47 +204,66 @@ public class TrabalhoPratico {
   }
 
   public static void main(String[] args) throws FileNotFoundException {
-    int[][] matrix = readInput();
+    Scanner scanner = new Scanner(new File(FILE_PATH));
 
-    System.out.println("b)");
-    printMatrix(matrix);
+    // Read the description of the terrain
+    scanner.nextLine();
 
-    System.out.println("c)");
-    addOffsetToSeaLevel(matrix, -1);
-    printMatrix(matrix);
+    int rows = scanner.nextInt();
+    int cols = scanner.nextInt();
 
-    System.out.println("d)");
-    double percentageOfFloodedMatrix = getPercentageFloodedMatrix(matrix);
-    System.out.printf("area submersa: %.2f", percentageOfFloodedMatrix);
-    System.out.println("%");
+    int[][] matrix = new int[rows][cols];
+    boolean valid = readInput(matrix, rows, cols, scanner);
 
-    System.out.println("e)");
-    double variationOfFloodedMatrix = getVariationOfFloodedMatrix(matrix);
-    System.out.printf("variacao da area inundada: %.2f m2%n", variationOfFloodedMatrix);
+    if (valid) {
+      System.out.println("b)");
+      printMatrix(matrix);
 
-    System.out.println("f)");
-    int floodedVolume = getFloodedVolume(matrix);
-    System.out.printf("volume da agua: %d m3%n", floodedVolume);
+      System.out.println("c)");
+      addOffsetToSeaLevel(matrix, -1);
+      printMatrix(matrix);
 
-    System.out.println("g)");
-    int maxHeight = getMaxHeight(matrix);
-    System.out.printf("para inundacao total :%d m%n", maxHeight + 1);
+      System.out.println("d)");
+      double percentageOfFloodedMatrix = getPercentageFloodedMatrix(matrix);
+      System.out.printf("area submersa: %.2f", percentageOfFloodedMatrix);
+      System.out.println("%");
 
-    System.out.println("h)");
-    int[] increments = getAreaIncrementsForFlooding(matrix, maxHeight);
-    printFloodingIncrements(increments);
+      System.out.println("e)");
+      double variationOfFloodedMatrix = getVariationOfFloodedMatrix(matrix);
+      System.out.printf("variacao da area inundada: %.2f m2%n", variationOfFloodedMatrix);
 
-    System.out.println("i)");
-    int[] data = getBestDirtRatioToFloodCube(matrix);
+      System.out.println("f)");
+      int floodedVolume = getFloodedVolume(matrix);
+      System.out.printf("volume da agua: %d m3%n", floodedVolume);
 
-    System.out.printf("coordenadas do cubo: (%d,%d), terra a mobilizar: %d m2%n", data[MAX_X_INDEX], data[MAX_Y_INDEX],
-        data[MOBILIZED_DIRT_INDEX]);
+      System.out.println("g)");
+      int maxHeight = getMaxHeight(matrix);
+      System.out.printf("para inundacao total: %d m%n", maxHeight + 1);
 
-    System.out.println("j)");
-    int dryColumn = getEasternDryColumn(matrix);
-    if (dryColumn == -1)
-      System.out.println("nao ha caminho seco na vertical");
-    else
-      System.out.printf("caminho seco na vertical na coluna (%d)%n", dryColumn);
+      System.out.println("h)");
+      int[] increments = getAreaIncrementsForFlooding(matrix, maxHeight);
+      printFloodingIncrements(increments);
+
+      System.out.println("i)");
+      if (hasValidDimensionsToFloodCube(matrix)) {
+        System.out.println("nao e possível posicionar um cubo nesta matriz");
+      } else {
+        int[] data = getBestDirtRatioToFloodCube(matrix);
+
+        System.out.printf("coordenadas do cubo: (%d,%d), terra a mobilizar: %d m2%n", data[MAX_X_INDEX],
+            data[MAX_Y_INDEX],
+            data[MOBILIZED_DIRT_INDEX]);
+      }
+
+      System.out.println("j)");
+      int dryColumn = getEasternDryColumn(matrix);
+
+      if (dryColumn == -1)
+        System.out.println("nao ha caminho seco na vertical");
+      else
+        System.out.printf("caminho seco na vertical na coluna (%d)%n", dryColumn);
+    } else {
+      System.out.println("matriz invalida");
+    }
   }
 }
